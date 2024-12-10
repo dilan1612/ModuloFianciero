8f35c8015a4ba01bf60bbffe845c89e3a96c3867
dilan1612 <93342327+dilan1612@users.noreply.github.com>
2024-12-09 20:52:23 -0500

    actualizacion

--- drivers/connect-db.js
+++ drivers/connect-db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const URI = "mongodb://localhost:27017"
+const URI = "https://cloud.mongodb.com/v2/66fc8c5ce5ffdb6e20748ad3#/metrics/replicaSet/66fc8cef232135128eb11989/explorer/Final/customers/find"
 
 mongoose.set('strictQuery', false)
 
